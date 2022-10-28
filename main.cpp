822fb50c966ea7b8623a777dad8c63da9c308660
Ignacio Franciosi <2104338@ucc.edu.ar>
2022-10-27 21:42:27 -0300

    sss

--- main.cpp
+++ main.cpp
@@ -10,22 +10,29 @@ using namespace std;
 
 
 int main(){
-    int tamanio=10;
     ifstream archivo;
     string textoArchivo;
-    HashMapList<string, Caracteres> MyHashMap(tamanio, &miHashFunc);
+   
+    
 
     archivo.open("texto.txt", ios::in); //in lectura on escritura
     while(!archivo.eof()){
         getline(archivo,textoArchivo);
-    }
 
+    }
+    HashMapList<string, Caracteres> MyHashMap(textoArchivo.length(), &miHashFunc);
+
+
+
+ /*
     Caracteres letras = {"a", 1};
     Caracteres palabras = {"juan",1 };
     
 
     MyHashMap.put(letras.caract, letras);
     MyHashMap.put(palabras.caract, palabras);
+    
+ */
     
     return 0;
     
