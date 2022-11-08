d0a5598f477499207dbc0fcec034f03fe2b7840b
leomorabito02 <leonardomorabito02@hotmail.com>
2022-11-08 13:28:06 -0300

    cambios

--- main.cpp
+++ main.cpp
@@ -6,23 +6,26 @@
 #include <algorithm>
 
 
-
 int main() {
-    
-ifstream archivo;
-string lectura;
-string prueba;
+
+    ifstream archivo;
+    string prueba;
 
 
-archivo.open("prueba.txt",ios::in);
+    archivo.open("archivoPrueba.txt", ios::in);
+    if(archivo.fail()){
+        cout<<"No se pudo abrir el archivo\n";
+    }
+    while (!archivo.eof()) {
+        getline(archivo, prueba);
+    }
 
-getline(archivo,prueba);
+    archivo.close();
+    cout << "txt: " << prueba << endl;
 
-archivo.close();
 
-    
     string caracts = ",.;:'-*+^=&%#$?¡¿!{[}]()";
-    for (char c: caracts){
+    for (char c: caracts) {
         prueba.erase(remove(prueba.begin(), prueba.end(), c), prueba.end());
     }
     stringstream input_stringstream(prueba);
@@ -32,16 +35,15 @@ archivo.close();
 /*
     HashIn ingreso = {"juan", 1};
 */
-int contador=0;
+    int contador = 0;
 
 
-while (getline(input_stringstream, lectura, ' '))
-    {
-        MyHashMap.put( lectura, 1);
+    while (getline(input_stringstream, prueba, ' ')) {
+        MyHashMap.put(prueba, 1);
         contador++;
     }
 
-    cout<<contador<<endl;
+    cout << contador << endl;
 
-    
+
 }
